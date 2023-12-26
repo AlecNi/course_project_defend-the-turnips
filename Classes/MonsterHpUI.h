a574e340529c4d84b112c71e02f3f8bd2dc34ac8
wbszzy <2690500175@qq.com>
2023-12-26 12:29:51 +0800

    nle 666

---
+++ Classes/MonsterHpUI.h
@@ -0,0 +1,31 @@
+/*
+2251918 刘骏伟 2023 12 25
+*/
+/*
+怪物血条类
+*/
+
+#ifndef MONSTERHPUI
+#define MONSTERHPUI
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+USING_NS_CC;
+
+class CMonsterHpUI : public Node {
+public:
+	CMonsterHpUI();
+	virtual ~CMonsterHpUI();
+
+	static CMonsterHpUI* create(const std::string& name);//创造血条显示
+
+	bool initWith();									//初始化
+
+	void showCurHealthRate(float flCurRate);			//更新血条
+
+protected:
+	CC_SYNTHESIZE(float, m_flMyHealthRate, MyHealthRate);//血条比
+	ui::LoadingBar* m_MonsterHealthBar;					//血条
+};
+
+
+#endif // !MONSTERHPUI
---
+++ Classes/MonsterMgr.h
@@ -0,0 +1,38 @@
+/*
+2251918 刘骏伟 2023 12 25 初版
+*/
+/*
+这是一个怪物管理器类，主要负责怪物的生成时机把握和波次控制
+*/
+#ifndef MONSTERMGR
+#define MONSTERMGR
+
+#include "cocos2d.h"
+#include "Monster.h"
+#include <vector>
+USING_NS_CC;
+
+struct SWaveData;
+
+class CMonsterMgr : public Layer {
+public:
+	CMonsterMgr();
+	virtual ~CMonsterMgr();
+
+	static CMonsterMgr* create(SWaveData& initData);
+
+	bool initWithData(SWaveData& initData);
+
+	void WaveStart();			//波次开始
+	void MonsterGenerate();		//怪物生成逻辑
+
+	bool isFinished();			//返回是否游戏已经结束
+protected:
+	CC_SYNTHESIZE(int, m_iCurWaveNum, CurWaveNum);	//表示当前波次
+	SWaveData* m_pWaveData;							//波次数据
+	std::vector<CMonster*> m_vecActiveMonsterList;	//活跃怪物池
+	std::vector<CMonster*> m_vecInActiveMonsterList; //不活跃怪物池
+};
+
+
+#endif // !MONSTERMGR
